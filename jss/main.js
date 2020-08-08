09ac6e72926fe263ba4ad3e48e5381219369e5bd
Tyler Reid <tylerreid1988@gmail.com>
2020-08-08 16:34:28 -0400

    Add reset button. Check auto restart issues

--- jss/main.js
+++ jss/main.js
@@ -5,23 +5,23 @@ const timerElFive = document.getElementById('five-btn')
 const timerElTen = document.getElementById('ten-btn')
 const secBar = document.getElementById('seconds')
 const minBar = document.getElementById('minutes')
+const resetBtn = document.getElementById('resetButton')
+
+
 //variables
 let timerInterval
 let min, sec, seconds = (300)
 let minTen, secTen, secondsTen = (600)
 //event listener 
   //click start button begin  5 min countdown
-   
-  
-  //FOR THE FIVE MINUTE TIMER
-  timerElFive.addEventListener('click', ()=> {
+   timerElFive.addEventListener('click', ()=> {
         if(timerInterval){
           clearInterval(timerInterval)
           return timerInterval = null
         }
         console.log(startTimer())
     })
-    //FOR THE API ON-CLICK OF FIVE MINUTE TIMER
+
     timerElFive.addEventListener('click', ()=>{
         fetch("https://api.adviceslip.com/advice")
         .then((response) =>{
@@ -30,13 +30,32 @@ let minTen, secTen, secondsTen = (600)
         .then((data)=>{
           let newQuote = {}
           newQuote.quote = data.slip.advice
-            document.appendChild(newQuote, quote)//quotes.push(newQuote)
+            quotes.push(newQuote)
             render()
         })
         .catch((err)=>{
             console.log(err)
         })
      })
+
+     resetBtn.addEventListener('click', () => {
+        clearInterval(timerInterval)
+        seconds = 300
+        clearInterval(timerInterval)
+        render()
+        return timerInterval = null
+    })
+
+
+
+    timerElTen.addEventListener('click', () => {
+        if(timerInterval){
+            clearInterval(timerInterval)
+            return timerInterval = null
+        }
+        startTimerTen()
+    })
+
 
     function tick(){
         seconds--
@@ -50,7 +69,7 @@ let minTen, secTen, secondsTen = (600)
         clearInterval(timerInterval)
     timerInterval = setInterval(tick, 1000)
     }
-        //five minute render function
+        //five minute
     function render() {
         min = Math.floor(seconds / 60) 
         sec = seconds % 60
@@ -65,14 +84,8 @@ let minTen, secTen, secondsTen = (600)
         // minBar.style = `width: ${(min/60*100)%60}%`
         // minBar.innerText = min
     }
-//FOR THE TEN MINUTE TIMER
-    timerElTen.addEventListener('click', () => {
-        if(timerInterval){
-            clearInterval(timerInterval)
-            return timerInterval = null
-        }
-        startTimerTen()
-    })
+
+
 
     function tickTen(){
         secondsTen--
@@ -97,5 +110,123 @@ let minTen, secTen, secondsTen = (600)
             timerElTen.innerText = `${minTen}:${secTen}`
         }
     }
+//     fetch("https://healthruwords.p.rapidapi.com/v1/quotes/?id=731&t=Wisdom&maxR=1&size=medium", {
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-host": "healthruwords.p.rapidapi.com",
+// 		"x-rapidapi-key": "SIGN-UP-FOR-KEY"
+// 	}
+// })
+// .then(response => {
+// 	console.log(response);
+// })
+// .catch(err => {
+// 	console.log(err);
+// });
+    
+    
+//     if (timerInterval){
+//         clearInterval(timerInterval)
+//         return timerInterval = null
+//     }
+//     startTimer()
+// })
+
+// tenButton.addEventListener('click', () =>{
+    // fetch("https://healthruwords.p.rapidapi.com/v1/quotes/?id=731&t=Wisdom&maxR=1&size=medium", {
+    //     "method": "GET",
+    //     "headers": {
+    //         "x-rapidapi-host": "healthruwords.p.rapidapi.com",
+    //         "x-rapidapi-key": "SIGN-UP-FOR-KEY"
+    //     }
+    // })
+    // .then(response => {
+    //     console.log(response);
+    // })
+    // .catch(err => {
+    //     console.log(err);
+    // });
+
+// })
 
 
+
+
+
+
+
+
+
+// function tick(){
+//     //seconds-- count downwards
+//     seconds--
+//     console.log(seconds)
+//     if (seconds === 0){
+//         clearInterval(timerInterval)
+//     }
+//     render()
+// }
+
+// function startTimer(){
+//     clearInterval(timerInterval)
+//     timerInterval = setInterval(tick, 1000)
+// }
+
+
+// function render() {
+//     min = Math.floor(seconds / 60);
+//     sec = seconds % 60;
+//     if (sec < 10) {
+//         timerEl.innerText = `${min}:0${sec}`
+//     } else {
+//         timerEl.innerText = `${min}:${sec}`
+//     }
+//     // secBar.style = `width: ${sec/60*100}%`
+//     // secBar.innerText = sec;
+//     // minBar.style = `width: ${(min/60*100)%60}%`
+//     // minBar.innerText = min;
+//     // hrBar.style = `width: ${(hr/24*100)%24}%`
+//     // hrBar.innerText = hr;
+// }
+
+
+
+
+
+
+
+  
+
+
+//   //function
+// //I want to use THIS function to set the timer to count backwards by 1 second (1000ms)
+// // setInterval(() => {
+// //     if(counter === 0){
+// //         return
+// //     }
+// // }, 1000);
+
+
+
+
+// // function tick(){
+// //     seconds--
+// //     console.log(seconds)
+// //     if (seconds === 0){
+// //         clearInterval(timerInterval)
+// //     }
+// //     render()
+// // }
+
+// //   function startTimer(){
+// // clearInterval(timerInterval)
+// // timerInterval = setInterval(tick, 1000)
+// //   }
+
+
+
+
+// //   function render() {
+    
+// //     }
+  
